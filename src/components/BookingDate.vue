d0f2885d5f1297ff3eee4a4b31ad0d3d262fb4f9
ROYWOODEN <ronald.brooks2006@gmail.com>
2025-08-20 19:28:14 +0300

    feat(date-zone): implement date/zone selection

---
+++ src/components/BookingDate.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    @click="emit('select')"
+    class="p-3 rounded-xl cursor-pointer font-bold"
+    :class="isActive ? 'bg-[#007AFF]' : 'bg-[#242426]'"
+  >
+    {{ day }}
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  day: string
+  isActive: boolean
+}
+const { day, isActive } = defineProps<Props>()
+const emit = defineEmits(['select'])
+</script>
+
+<style scoped></style>
